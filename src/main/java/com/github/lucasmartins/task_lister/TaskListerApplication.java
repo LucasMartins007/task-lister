ce930f8e213fe2c1398a7462a9faed09b7d786f6
Lucas Martins <lucasrodriguesmartinsoliveira@gmail.com>
2025-02-08 01:10:43 -0300

    Adicionando configurações iniciais do pom da aplicacao

---
+++ src/main/java/com/github/lucasmartins/task_lister/TaskListerApplication.java
@@ -0,0 +1,13 @@
+package com.github.lucasmartins.task_lister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskListerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskListerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/lucasmartins/task_lister/TaskListerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.github.lucasmartins.task_lister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskListerApplicationTests {
+
+/*
+	@Test
+	void contextLoads() {
+	}
+*/
+
+}
