fe07a2fc4db7e1f2611eefcebc1d41fadb36ebc4
Lucas Martins <lucasrodriguesmartinsoliveira@gmail.com>
2025-02-09 01:30:19 -0300

    implementação inicial da configuração de segurança da api

---
+++ src/main/java/com/github/lucasmartins/task_lister/properties/SecurityProperties.java
@@ -0,0 +1,15 @@
+package com.github.lucasmartins.task_lister.properties;
+
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+@ConfigurationProperties(prefix = "spring.security.jwt")
+public class SecurityProperties {
+
+    private String secret;
+
+    private String expiration;
+}
